cd9baa57dca8bd54fa975a3596216b74ef7b6c3f
Brooks J Rady <b.j.rady@gmail.com>
2020-10-27 17:21:19 +0000

    Add a new test

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,3 +6,9 @@ main = hspec $ do
   describe "dominos" $ do
     it "should be 28 dominos in total" $ do
       length dominos `shouldBe` 28
+
+  describe "possPlays" $ do
+    let board = [(5,2),(2,4),(4,4),(4,0)]
+    it "returns possibilities on both ends of the board" $ do
+      let hand = [(6,1),(6,5),(3,3),(0,0)]
+      (possPlays board hand) `shouldBe` ([(6,5)],[(0,0)])
