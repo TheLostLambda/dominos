b503b9377100da0342868929523b51f367985b2b
Brooks J Rady <b.j.rady@gmail.com>
2020-10-27 13:47:33 +0000

    Working but messy

--- Dominos.hs
+++ Dominos.hs
@@ -12,9 +12,15 @@ type Hand = [Domino]
 
 -- A `Board` has two open ends and a list of the `Domino`s that have been played
 data Board = Board Int [Domino] Int
+  deriving Show
 
 -- Only two ends in our implementation
 data End = Left | Right
+  deriving Show
+
+-- Generate a list of all possible `Domino`s
+dominos :: [Domino]
+dominos = [[a,b] | a <- [0..6], b <- [a..6]]
 
 
 -- Can a given domino be played at a given end of the board?
@@ -31,18 +37,14 @@ played (Board _ played _) = (`elem` played)
 
 -- Attempt to play the given domino at one end of a board
 playDom :: Board -> End -> Domino -> Maybe Board
-playDom board end domino@[a,b]
-  | canPlay board end domino = Just $ atEnd play board end
+playDom board@(Board l p r) end dom@[a,b]
+  | canPlay board end dom = Just $ play end
   | otherwise = Nothing
-  where play e = if a == b then a + b else sum $ filter (/=e) domino
-          
--- FIXME: Boilerplate for days...
--- Run a function on the given end of a board
-atEnd :: (Int -> Int) -> Board -> End -> Board
-atEnd f (Board l p r) Left = Board (f l) p r
-atEnd f (Board l p r) Right = Board l p (f r)
+  where play Left  = Board newEnd (dom:p) r
+        play Right = Board l (dom:p) newEnd
+        newEnd = if a == b then a + b else sum $ filter (/= getEnd board end) dom
 
--- Get the given end of a board
+-- Get the given `End` of the `Board`, adjusted for doubles
 getEnd :: Board -> End -> Int
-getEnd (Board l _ _) Left  = l
-getEnd (Board _ _ r) Right = r
+getEnd (Board l _ r) end = if pips > 6 then pips `div` 2 else pips
+  where pips = case end of { Left -> l; Right -> r }
