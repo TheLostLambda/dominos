33882e3f6f24082503ceee9a7bfae9d7cc99d477
Brooks J Rady <b.j.rady@gmail.com>
2020-10-27 14:43:16 +0000

    Different system!

--- Dominos.hs
+++ Dominos.hs
@@ -3,48 +3,47 @@
   Brooks Rady, October 2020
 -}
 
--- Hide the `Left` and `Right` data constructors from `Either`
-import Prelude hiding (Left, Right)
-
 -- Simple aliases
-type Domino = [Int] -- FIXME: (Int, Int)?
+type Domino = [Int]
 type Hand = [Domino]
-
--- A `Board` has two open ends and a list of the `Domino`s that have been played
-data Board = Board Int [Domino] Int
-  deriving Show
+type Board = [Domino]
 
 -- Only two ends in our implementation
-data End = Left | Right
-  deriving Show
+data End = L | R
 
 -- Generate a list of all possible `Domino`s
 dominos :: [Domino]
 dominos = [[a,b] | a <- [0..6], b <- [a..6]]
 
+-- Has a given domino already been played?
+played :: Board -> Domino -> Bool
+played = flip elem
 
 -- Can a given domino be played at a given end of the board?
 canPlay :: Board -> End -> Domino -> Bool
-canPlay board end = any (== getEnd board end)
+canPlay board end = any (== getEnd end board)
+
+-- Return a tuple of all possible plays on either end of the board
+possPlays :: Board -> Hand -> ([Domino], [Domino])
+possPlays board hand = (plays L, plays R)
+  where plays end = [d | d <- hand, canPlay board end d]
 
 -- Is the player 'knocking' (nothing can be played)?
 blocked :: Board -> Hand -> Bool
-blocked board hand = not $ or [canPlay board e d | e <- [Left, Right], d <- hand]
-
--- Has a given domino already been played?
-played :: Board -> Domino -> Bool
-played (Board _ played _) = (`elem` played)
+blocked board hand = null $ l ++ r
+  where (l, r) = possPlays board hand
 
 -- Attempt to play the given domino at one end of a board
 playDom :: Board -> End -> Domino -> Maybe Board
-playDom board@(Board l p r) end dom@[a,b]
-  | canPlay board end dom = Just $ play end
+playDom board end [a,b]
+  | canPlay board end [a,b] = Just $ play end
   | otherwise = Nothing
-  where play Left  = Board newEnd (dom:p) r
-        play Right = Board l (dom:p) newEnd
-        newEnd = if a == b then a + b else sum $ filter (/= getEnd board end) dom
+  where play L = reverse newEnd : board
+        play R = board ++ [newEnd]
+        newEnd = if a == getEnd end board then [a,b] else [b,a]
 
--- Get the given `End` of the `Board`, adjusted for doubles
-getEnd :: Board -> End -> Int
-getEnd (Board l _ r) end = if pips > 6 then pips `div` 2 else pips
-  where pips = case end of { Left -> l; Right -> r }
+-- Get the given `End` of the `Board`
+getEnd :: End -> Board -> Int
+getEnd L = head . head
+getEnd R = last . last
+
