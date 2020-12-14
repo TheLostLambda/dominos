7178263a36ec3f150dbb3712756cae542c082e4f
Brooks J Rady <b.j.rady@gmail.com>
2020-12-14 03:17:59 +0000

    Style fixes

--- src/AI.hs
+++ src/AI.hs
@@ -6,88 +6,109 @@
 
 module AI where
 
--- FIXME: More list comps, less map and filter
-
-import Data.List
-import Data.Ord
-import Data.Tuple
+import Data.List (delete, foldl', group, intersect, maximumBy, sort, union, (\\))
+import Data.Ord (comparing)
 import DomsMatch
 
-{- Data Types ----------------------------------------------------------------}
+{- Data Types ------------------------------------------------------------------------------------}
 
-data GameState = GS Hand DominoBoard Player Scores -- FIXME: Make me a record type?
+-- Wraps all of the args of `DomsPlayer` into a single, easy-to-pass value
+data GameState = GS Hand DominoBoard Player Scores
+
+-- Tactics score a list of plays according to strategic advantage they provide
+type Tactic = GameState -> [Play] -> [(Play, Points)]
+
+-- Miscellaneous type synonyms for clarity
 type Play = (Domino, End)
 type Points = Int
 type Pip = Int
-type Tactic = GameState -> [Play] -> [(Play, Points)]
 
-{- Public AI Players ---------------------------------------------------------}
+{- Public AI Players -----------------------------------------------------------------------------}
 
+-- Simply play the highest scoring domino possible
 playerH :: DomsPlayer
 playerH = strategy [highScoring]
 
+-- Prioritize (5,4) as the first domino to drop
 playerHF :: DomsPlayer
 playerHF = strategy [highScoring, firstDrop]
 
+-- Past scores of 50, look for plays that land on 59 (many ways to win) or win outright
 playerHFE :: DomsPlayer
 playerHFE = strategy [highScoring, firstDrop, endGame]
 
+-- Discourage dropping dangerous dominos – (6,6) and (5,5) – if they can't be knocked off
 playerHFEB :: DomsPlayer
 playerHFEB = strategy [highScoring, firstDrop, endGame, blindDanger]
 
+-- Encourage the dropping of dominos that expose pips that the player holds the majority of
 playerHFEBM :: DomsPlayer
 playerHFEBM = strategy [highScoring, firstDrop, endGame, blindDanger, mostPips]
 
+-- Predict the opponent's hand and discourage plays that are likely to yield them points
 playerHFEBMS :: DomsPlayer
 playerHFEBMS = strategy [highScoring, firstDrop, endGame, blindDanger, mostPips, smartDanger]
 
-{- AI Player Tactics ---------------------------------------------------------}
+{- AI Player Tactics -----------------------------------------------------------------------------}
 
--- FIXME: Could use some spice
+-- Zip each play with the points it would score
 highScoring :: Tactic
 highScoring (GS _ board _ _) = map (\p -> (p, scorePlay board p))
 
+-- If the board is empty, encourage dropping (5,4)
+-- NB: Adding 2 to the existing (5,4) score of 3, puts it above the score of any other first play
 firstDrop :: Tactic
 firstDrop (GS _ InitBoard _ _) plays
-    | ((5, 4), L) `elem` plays = [(((5, 4), L), 2)]
+    | ((5, 4), L) `elem` plays = [(((5, 4), L), 2)] -- If (5,4) is present, L is always possible
     | otherwise = []
 firstDrop _ _ = []
 
+-- Check if it's possible to either win the game or get to 59, *strongly* encouraging those plays
 endGame :: Tactic
 endGame gs@(GS _ board _ _) plays
-    | playerScore gs > 50 = zip (getTo 61) (repeat 20) ++ zip (getTo 59) (repeat 10)
+    | playerScore gs > 50 = zip (getTo 61) (repeat 30) ++ zip (getTo 59) (repeat 15)
     | otherwise = []
   where
     getTo score = intersect plays $ scoreN board (score - playerScore gs)
 
+-- Discourage dropping "dangerous" dominos if they can't be knocked off
+-- NB: The dangerous dominos – (6,6) and (5,5) – and the penalty of -2 were determined empirically
 blindDanger :: Tactic
-blindDanger (GS hand _ _ _) plays = map (\p -> (p, -2)) dangerPlays
+blindDanger (GS hand _ _ _) = map (\p -> (p, -2)) . dangerPlays
   where
-    dangerPlays = filter (\(d@(n, _), _) -> d `elem` dangerDoms && n `notElem` knockable) plays
-    knockable = concatMap (\(a, b) -> [a, b]) $ hand \\ dangerDoms
+    -- Find plays with dangerous dominos and unowned pips
+    dangerPlays = filter (\(d@(n, _), _) -> d `elem` dangerDoms && n `notElem` ownedPips)
+    -- Find all pips in the player's hand (excluding the dangerous dominos themselves)
+    ownedPips = concatMap (\(a, b) -> [a, b]) $ hand \\ dangerDoms
     dangerDoms = [(6, 6), (5, 5)]
 
-mostPips :: Tactic -- 1 is better than c - 4, empirically
-mostPips (GS hand board _ _) plays = [(pl, 1) | (c, p) <- countPips hand, c > 4, pl@(d, _) <- plays, exposesPip pl p]
-  where
-    exposesPip play@(d, _) pip
-        | board == InitBoard = d `hasPip` pip
-        | otherwise = newEnd board play == pip
+-- Encourage plays that expose pips the player "controls"
+-- NB: "Controlled" pips are those that the player owns the majority of (more than 4)
+mostPips :: Tactic
+mostPips (GS hand board _ _) plays =
+    [(pl, c - 4) | (c, p) <- countPips hand, c > 4, pl <- plays, p `elem` newEnds' board pl]
 
+-- Given what the opponent could be holding, calculate the average risk of each play
+-- NB: Risk is a scaled average of all of the points an opponent could score in response to a play
 smartDanger :: Tactic
 smartDanger gs@(GS _ board player _) = map (\p -> (p, - dangerScore p))
   where
-    oHand = otherHand gs
-    oHandOdds = fromIntegral (handSize gs $ other player) / fromIntegral (length oHand)
-    dangerScore (dom, end) = round $ sum (map (\x -> mean [scorePlay newBoard p | p <- allPlays newBoard [x]]) oHand) * oHandOdds
+    oHand = otherHand gs -- All of the dominos that an opponent could have
+    oHandOdds = otherHandSize gs // length oHand -- How likely they are to hold each of those
+    -- Simulate every play and calculate what the opponent could score in response
+    dangerScore (domino, end) = round $ sum (map domRisk oHand) * oHandOdds -- Scale by confidence
       where
-        Just newBoard = playDom player dom board end
+        -- For every opponent domino, calculate the retaliation score – averaging L and R plays
+        domRisk d = mean . map (scorePlay newBoard) $ allPlays newBoard [d]
+        -- Peek at a possible future (the play is known to be legal)
+        Just newBoard = playDom player domino board end
 
-{- Private Helper Functions --------------------------------------------------}
+{- Player Tactic Composition ---------------------------------------------------------------------}
 
 -- FIXME: Lots, and maybe sortOn?
 strategy :: [Tactic] -> DomsPlayer
-strategy tactics hand board player score = fst . maximumBy (comparing snd) $ foldl' applyTactic initPlays tactics
+strategy tactics hand board player score =
+    fst . maximumBy (comparing snd) $ foldl' applyTactic initPlays tactics
   where
     gs = GS hand board player score
     initPlays = map (\p -> (p, 0)) $ allPlays board hand
@@ -99,66 +120,74 @@ mergePlays = foldl' insertPlay
     insertPlay plays new@(d, p) =
         maybe new (\x -> (d, p + x)) (lookup d plays) : filter (\(x, _) -> x /= d) plays
 
-mean :: (Integral a, Fractional b) => [a] -> b
-mean [] = 0.0
-mean lst = fromIntegral (sum lst) / fromIntegral (length lst)
+{- Opponent Prediction ---------------------------------------------------------------------------}
 
--- FIXME: Spice this up with the applicative of functions? Curry?
+otherHandSize :: GameState -> Int
+otherHandSize (GS _ InitBoard _ _) = num_in_hand
+otherHandSize (GS _ (Board _ _ history) player _) =
+    num_in_hand - length [p | (_, p, _) <- history, p /= player]
+
+-- FIXME: Yuck
+otherHand :: GameState -> [Domino]
+otherHand (GS hand InitBoard _ _) = domSet \\ hand
+otherHand (GS hand board@(Board _ _ history) player _) = filter (not . hasKnockingPip) unknownDoms
+  where
+    unknownDoms = filter (not . flip played board) $ domSet \\ hand
+    hasKnockingPip d = any (d `hasPip`) $ knockingPips history player
+    knockingPips [] _ = []
+    knockingPips hist pl
+        | pl == player && pl == lastPl = knockingPips (delete lastPlay hist) lastPl `union` endPips hist
+        | otherwise = knockingPips (delete lastPlay hist) lastPl
+      where
+        lastPlay@(_, lastPl, _) = maximumBy (comparing $ \(_, _, x) -> x) hist
+
+{- Domino Helper Functions -----------------------------------------------------------------------}
+
+-- Fetch the score of the current player
+playerScore :: GameState -> Points
+playerScore (GS _ _ P1 (p, _)) = p
+playerScore (GS _ _ P2 (_, p)) = p
+
+-- Return a list of all possible plays, given some board and hand
 allPlays :: DominoBoard -> Hand -> [Play]
 allPlays board hand = zip l (repeat L) ++ zip r (repeat R)
   where
     (l, r) = possPlays hand board
 
+-- Simulate a play and calculate its score
 scorePlay :: DominoBoard -> Play -> Points
 scorePlay board (domino, end) = scoreBoard possBoard
   where
     Just possBoard = playDom P1 domino board end
 
-playerScore :: GameState -> Points
-playerScore (GS _ _ P1 (p, _)) = p
-playerScore (GS _ _ P2 (_, p)) = p
+-- Simulate a play and return the new ends of the board
+newEnds' :: DominoBoard -> Play -> [Pip]
+newEnds' board (domino, end) = [l, r]
+  where
+    Just (Board (l, _) (_, r) _) = playDom P1 domino board end
 
--- FIXME: Bit repetitive here, innit?
-newEnd :: DominoBoard -> Play -> Pip
-newEnd (Board (l, _) _ _) ((f, s), L) = if f == l then s else f
-newEnd (Board _ (_, r) _) ((f, s), R) = if f == r then s else f
-
-hasPip :: Domino -> Pip -> Bool
-hasPip (f, s) p = p == f || p == s
-
-countPips :: [Domino] -> [(Int, Pip)]
-countPips = map (\x -> (length x, head x)) . group . sort . concatMap (\(f, s) -> [f, s])
-
-playedDoms :: History -> [Domino]
-playedDoms = concatMap (\(d, _, _) -> [d, swap d])
-
--- FIXME: Pointfree with a lambda?
+-- Find the free ends of a domino board at some point in the past
 endPips :: History -> [Pip]
 endPips history = [fst $ head board, snd $ last board]
   where
-    board = playedDoms history
+    board = [d | (d, _, _) <- history]
 
-other :: Player -> Player
-other P1 = P2
-other P2 = P1
+-- Check if a domino contains a given pip
+hasPip :: Domino -> Pip -> Bool
+hasPip (f, s) p = p == f || p == s
 
-handSize :: GameState -> Player -> Int
-handSize (GS _ InitBoard _ _) _ = num_in_hand
-handSize (GS _ (Board _ _ hist) _ _) player = num_in_hand - length [p | (_, p, _) <- hist, p == player]
+-- Convert a list of dominos into a count of their pips
+-- The sort-group-map approach is my favorite trick for generating a frequency list ;)
+countPips :: [Domino] -> [(Int, Pip)]
+countPips = map (\x -> (length x, head x)) . group . sort . concatMap (\(f, s) -> [f, s])
 
--- FIXME: Yuck
-knockingPips :: GameState -> [Pip]
-knockingPips (GS _ (Board _ _ hist) player _) = go hist player
-  where
-    go [] _ = []
-    go hist pl
-        | pl == player && pl == p = go (delete last hist) p `union` endPips hist
-        | otherwise = go (delete last hist) p
-      where
-        last@(_, p, _) = maximumBy (comparing $ \(_, _, x) -> x) hist
+{- General Helper Functions ----------------------------------------------------------------------}
 
-otherHand :: GameState -> [Domino]
-otherHand (GS hand InitBoard _ _) = domSet \\ hand
-otherHand gs@(GS hand (Board _ _ hist) _ _) = filter (\d -> not $ any (d `hasPip`) $ knockingPips gs) unknownDoms
-  where
-    unknownDoms = domSet \\ (hand ++ playedDoms hist)
+-- Performs the floating-point division of integers
+(//) :: (Integral a, Integral b, Fractional c) => a -> b -> c
+x // y = fromIntegral x / fromIntegral y
+
+-- Calculates the average value of a list
+mean :: (Integral a, Fractional b) => [a] -> b
+mean [] = 0.0
+mean lst = sum lst // length lst
